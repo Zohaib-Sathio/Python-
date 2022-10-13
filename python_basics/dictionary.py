36024bf8c1dd35980d3d7894a8ee377a70b43b14
Zohaib Hussain <zohaibhussainsathio@gmail.com>
2022-10-13 17:21:32 +0500

    added dictionary code

---
+++ python_basics/dictionary.py
@@ -0,0 +1,16 @@
+#basic definition
+Dic = {1:"Zohaib", 2:"Hussain", 3 : "Sathio"}
+i = 1
+for i in range(4):
+    print(Dic.get(i))
+print(Dic)
+
+#Mixed keys
+Dic2 = {5 : "Second definition", "Name" : "Zohaib"}
+print(Dic2)
+
+#For loop traversal
+Dic3 = {1:"Zohaib", 2:"Hussain", 3 : "Sathio"}
+i = 1
+for i in range(4):
+    print(Dic3.get(i))

62552687e6165df43a9f6d5c083c3c4a957c6e52
Zohaib Hussain <zohaibhussainsathio@gmail.com>
2022-10-18 20:53:41 +0500

    comments in python

---
+++ python_basics/python_comment.py
@@ -0,0 +1,13 @@
+# This is a single line comment in python
+
+'''
+    This is a multi line comment
+    You can write anything 
+    in between this block
+
+'''
+
+print("A program simply showing how to use comments in Python")
+
+
+print("Hello Zohaib! Get well soon!!!")

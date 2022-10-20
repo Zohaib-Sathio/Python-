ded2f0449f53e95d41fbd33b16770e2cdf9bf476
Zohaib Hussain <zohaibhussainsathio@gmail.com>
2022-10-20 18:52:56 +0500

    calling parent class constructor using super

--- python_basics/inheritance.py
+++ python_basics/inheritance.py
@@ -19,3 +19,21 @@ class Cat(Animal):
 
 cat = Cat('Lazi', True)
 cat.sound()
+
+
+
+# Another child class
+class Dog(Animal):
+    def __init__(self, name, isTamed, breed):
+        self.breed = breed
+        super().__init__(name, isTamed)
+
+    def full_info(self):
+        print(f'Name of the animal is {self.name}')
+        print(f'Is animal tamed? : {self.tamed}')
+        print(f'Breed of the dog is: {self.breed}')
+    
+
+dog = Dog('Wasay :)', False, 'Bull Dog')
+dog.display()
+dog.full_info()

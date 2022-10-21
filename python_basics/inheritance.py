35fc321efcc4ff2f533bdc9bba791865becdd55d
Zohaib Hussain <zohaibhussainsathio@gmail.com>
2022-10-21 20:41:59 +0500

    multiple inheritance added

--- python_basics/inheritance.py
+++ python_basics/inheritance.py
@@ -10,6 +10,13 @@ class Animal():
     
 animal = Animal('Bully', True)
 animal.display()
+
+class Age():
+    def __init__(self, age):
+        self.age = age
+    
+    def show_age(self):
+        print(f'Age is : {self.age}')
         
 # Child Class
 class Cat(Animal):
@@ -37,3 +44,18 @@ class Dog(Animal):
 dog = Dog('Wasay :)', False, 'Bull Dog')
 dog.display()
 dog.full_info()
+
+
+class LivingThing(Age, Animal):
+    def __init__(self, name, isTamed, age):
+        Animal.__init__(self, name, isTamed)
+        Age.__init__(self, age)
+    
+    def show_info_living_thing(self):
+        print('Child Class Method')
+
+
+species = LivingThing('Bunty', True, 10)
+species.display()
+species.show_age()
+species.show_info_living_thing()
